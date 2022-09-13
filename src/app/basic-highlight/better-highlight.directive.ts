967d4812ce65463e568e4e0aeb2d916b1faf6b12
saishree <Saishree@leadwinner.com>
2022-09-13 12:20:43 +0530

    using hostlistener to listen to events on the element and using host binding to bind to propert of the elemnet

--- src/app/basic-highlight/better-highlight.directive.ts
+++ src/app/basic-highlight/better-highlight.directive.ts
@@ -1,10 +1,15 @@
-import { Directive,ElementRef,HostListener,OnInit, Renderer2 } from '@angular/core';
+import { parseHostBindings } from '@angular/compiler';
+import { Directive,ElementRef,HostBinding,HostListener,OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+
+  @HostBinding('style.backgroundColor') backgroundColor :string ='transparent'
+
+  
 
 
   constructor( private item:ElementRef,private renderer:Renderer2) { }
@@ -14,13 +19,13 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseOnHover(eventData:Event){
-    this.renderer.setStyle(this.item.nativeElement,'background-color','blue');
-
+    // this.renderer.setStyle(this.item.nativeElement,'background-color','blue');
+   this.backgroundColor ='blue';
   }
 
   @HostListener('mouseleave') mouseOnMovingAside(eventData:Event){
-    this.renderer.setStyle(this.item.nativeElement,'background-color','transparent');
-
+    // this.renderer.setStyle(this.item.nativeElement,'background-color','transparent');
+    this.backgroundColor ='transparent';
   }
 
 }
